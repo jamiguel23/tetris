01ec45bbb3fca8f16c94a8e1360832edf598ea35
Matt <josematthewmiguel@gmail.com>
2022-09-05 23:05:25 -0700

    query selected start button and score

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ document.addEventListener('DOMContentLoaded', () => {
   const gird = document.querySelector('.grid')
   let squares = Array.from(document.querySelectorAll('.grid div'))
   const width = 10
+  const scoreDisplay = document.querySelector('#score')
+  const startBtn =document.querySelector('#start-button')
 
 
+  console.log(scoreDisplay, startBtn)
 })
