d40c3de6457be1f6afe32973b61bc51fcbf90433
Matt <josematthewmiguel@gmail.com>
2022-09-07 17:08:05 -0700

    added tetros

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 'use strict'
 
-let app = document.addEventListener('DOMContentLoaded', () => {
+
+document.addEventListener('DOMContentLoaded', () => {
 
   const gird = document.querySelector('.grid')
   let squares = Array.from(document.querySelectorAll('.grid div'))
@@ -9,7 +10,40 @@ let app = document.addEventListener('DOMContentLoaded', () => {
   const startBtn =document.querySelector('#start-button')
 
 
-  console.log(scoreDisplay, startBtn)
+  const lTetro = [
+    [1, width+1, width*2+1, 2],
+    [width, width+1, width+2, width*2+2],
+    [1, width+1, width*2+1, width*2],
+    [width, width*2, width*2+1, width*2+2] 
+  ]
+
+  const zTetro = [
+    [0, width, width+1, width*2+1],
+    [width+1, width+2, width*2, width*2+1],
+    [0, width, width+1, width*2+1],
+    [width+1, width+2, width*2, width*2+1]
+  ]
+
+  const tTetro = [
+    [1, width, width+1, width+2],
+    [1, width+1, width+2, width*2+1],
+    [width, width+1, width+2, width*2+1],
+    [1, width, width+1, width*2+1]
+  ]
+
+  const oTetro = [
+    [0,1,width,width+1],
+    [0,1,width,width+1],
+    [0,1,width,width+1],
+    [0,1,width,width+1]
+  ]
+
+  const iTetro = [
+    [1, width+1, width*2+1, width*3+1],
+    [width, width+1, width+2, width+3],
+    [1, width+1, width*2+1, width*3+1],
+    [width, width+1, width+2, width+3]
+  ]
+
 })
 
-module.exports = app
